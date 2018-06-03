311c2a57b07b00b66364992d99ac9a7a05be5107
ToBeAmped <martinja@oregonstate.edu>
2018-06-03 12:16:00 -0700

    Add endpoint for returning the quotient of two numbers

--- app/server.js
+++ app/server.js
@@ -10,4 +10,8 @@ app.get("/say/the-sum", function(req, res) {
   res.send(say.theSum(req.query.left, req.query.right));
 });
 
+app.get("/say/the-quotient", function(req, res) {
+  res.send(say.theQuotient(req.query.left, req.query.right));
+});
+
 app.listen(3000);
